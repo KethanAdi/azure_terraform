6fa978d60f692145abab1f73af0079f16e0558b2
KethanAdi <102666849+KethanAdi@users.noreply.github.com>
2022-04-14 18:55:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider (azurerm provider) in provider blocks.
-
 provider "azurerm" {
-  features {}
+  version = "=3.1.0"
 }
